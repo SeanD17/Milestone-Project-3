12aada62926fa0e2cc85b23d28a0ce1a1a8fdb19
Sean Dolan <seandolan53@yahoo.com>
2021-08-11 16:54:54 +0000

    Change to App py Added code to display the book details a user has created from the MongoDB site and display these details on the profile page

--- app.py
+++ app.py
@@ -93,7 +93,8 @@ def profile(username):
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        books = list(mongo.db.books.find())
+        return render_template("profile.html", username=username, books=books)
 
     return redirect(url_for("login"))
 
